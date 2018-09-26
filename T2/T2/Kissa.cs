6cad516b860029912640030b64afb0a2f5c29d1b
Arttu Höylä <aahoo01@gmail.com>
2018-09-26 13:20:30 +0300

    Completed T2

---
+++ T2/T2/Kissa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Kissa
+    {
+        private int ika;
+        public String nimi;
+
+        public Kissa() { }
+
+        public Kissa(int ika, String nimi)
+        {
+            this.ika = ika;
+            this.nimi = nimi;
+        }
+
+        public void asetaKissanNimi(String nimi)
+        {
+            this.nimi = nimi;
+        }
+
+        public String PalautaKissanNimi()
+        {
+            return nimi;
+        }
+
+        public int PalautaKissanika()
+        {
+            return ika;
+        }
+    }
+}
---
+++ T2/T2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kissa kissa = new Kissa(int.MinValue,"Numa");
+
+            Console.WriteLine(kissa.PalautaKissanika());
+            Console.WriteLine(kissa.PalautaKissanNimi());
+            Console.ReadKey();
+        }
+    }
+}
