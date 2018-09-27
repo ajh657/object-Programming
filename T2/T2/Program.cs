63ddf9e1201cc4610578b02a4e448300530d6696
Arttu Höylä <aahoo01@gmail.com>
2018-09-27 10:23:32 +0300

    Completed T5

--- T2/T2/Program.cs
+++ T2/T2/Program.cs
@@ -11,6 +11,8 @@ namespace T2
         static void Main(string[] args)
         {
             Kissa kissa = new Kissa();
+            Koira koira = new Koira();
+
             Console.WriteLine("Anna Kissalle nimi");
             if (kissa.asetaKissanNimi(Console.ReadLine()))
             {
@@ -23,7 +25,20 @@ namespace T2
                 Console.WriteLine("Ei Onistunut: Nimi ei saa olla Hilda");
             }
 
+            Console.WriteLine("Anna Koiralle nimi");
+            if (koira.asetaKoiranNimi(Console.ReadLine()))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Onnistui");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ei Onistunut: Nimi ei saa olla Haukkuli");
+            }
+
             Console.WriteLine();
+
             Console.WriteLine(kissa.PalautaKissanika());
             Console.WriteLine(kissa.PalautaKissanNimi());
             Console.ReadKey();
