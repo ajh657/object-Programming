fb6b736b638ac8fb95ff940b504adf36a46b7c2b
Arttu Höylä <aahoo01@gmail.com>
2018-10-01 12:18:22 +0300

    Started T12

---
+++ T12/T12/Query.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T12
+{
+    public partial class Query : Form
+    {
+        SQL DataBase = new SQL();
+        public Query()
+        {
+            InitializeComponent();
+            DataBase.OpenConnection();
+        }
+
+        private void Query_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ T12/T12/SQL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace T12
+{
+    class SQL
+    {
+        SqlConnection MyConnection;
+        private void CreateConnection()
+        {
+            try
+            {
+                MyConnection = new SqlConnection("Data Source=185.101.93.152;Integrated Security=False;User ID=SA;Password=Voimamiina-12;Application Name=30");
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show("Connection Error:" + e);
+            }
+        }
+
+        public void OpenConnection()
+        {
+            CreateConnection();
+
+            try
+            {
+                MyConnection.Open();
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show("Connection Error:" + e);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (MyConnection != null) MyConnection.Close();
+        }
+    }
+}
